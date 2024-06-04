437a4562d6f3761a64c85961f882c86151e5a031
JeromeBslz <jerome933@outlook.com>
2024-06-04 15:57:41 +0200

    Deuxieme commit route

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,15 +1,32 @@
 const express = require("express");
 
+
 const router = express.Router();
 
+
+const { sayWelcome } = require("../../controllers/sayActions");
+
+
+router.get("/", sayWelcome);
+
+
 /* ************************************************************************* */
+
 // Import And Use Routers Here
+
 /* ************************************************************************* */
+
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
 
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter)
+
+
 /* ************************************************************************* */
+
 
 module.exports = router;
